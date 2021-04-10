c089a3d957da7098a83f11c2081ae598e626091f
tim <tim@glo.id.au>
2021-04-11 09:28:05 +1000

    inject css into body, and not the document.head

--- src/NotFoundPage.cy.test.tsx
+++ src/NotFoundPage.cy.test.tsx
@@ -1,35 +1,29 @@
-import { expect, describe, it, cy } from "local-cypress";
+import { expect, describe, it, cy, beforeEach } from "local-cypress";
 import React from "react";
 import { mount } from "@cypress/react";
+import { sheet } from "@emotion/css";
 import { Box, colors } from "@imtbl/design-system";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
 
 import NotFoundPage from "./NotFoundPage";
 
-const cssCache = createCache({
-  key: "mooo-cow",
-  container: document.body,
-});
-
 describe("<AssetDetailsContent />", () => {
+  beforeEach(() => {
+    sheet.container = document.body;
+  });
+
   it("test 1", () => {
     mount(
-      <CacheProvider value={cssCache}>
-        <Box backgroundColor={colors.bg[700]} padding="30px">
-          <NotFoundPage />
-        </Box>
-      </CacheProvider>
+      <Box backgroundColor={colors.bg[700]} padding="30px">
+        <NotFoundPage />
+      </Box>
     );
   });
 
   it("test 2", () => {
     mount(
-      <CacheProvider value={cssCache}>
-        <Box backgroundColor={colors.bg[700]} padding="30px">
-          <NotFoundPage />
-        </Box>
-      </CacheProvider>
+      <Box backgroundColor={colors.bg[700]} padding="30px">
+        <NotFoundPage />
+      </Box>
     );
   });
 });
