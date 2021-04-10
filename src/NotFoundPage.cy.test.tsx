53609efd920b268068d43a562660a863f5829707
tim <tim@glo.id.au>
2021-04-10 13:16:55 +1000

    trying out use of emotion cacheProvider, no dice. :(

--- src/NotFoundPage.cy.test.tsx
+++ src/NotFoundPage.cy.test.tsx
@@ -1,24 +1,36 @@
 import { expect, describe, it, cy } from "local-cypress";
 import React from "react";
 import { mount } from "@cypress/react";
+import { Box, colors } from "@imtbl/design-system";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 import NotFoundPage from "./NotFoundPage";
-import { Box, colors } from "@imtbl/design-system";
+
+const cssCache = createCache({
+  key: "mooo-cow",
+  container: document.body,
+  prepend: true,
+});
 
 describe("<AssetDetailsContent />", () => {
   it("test 1", () => {
     mount(
-      <Box backgroundColor={colors.bg[700]} padding="30px">
-        <NotFoundPage />
-      </Box>
+      <CacheProvider value={cssCache}>
+        <Box backgroundColor={colors.bg[700]} padding="30px">
+          <NotFoundPage />
+        </Box>
+      </CacheProvider>
     );
   });
 
   it("test 2", () => {
     mount(
-      <Box backgroundColor={colors.bg[700]} padding="30px">
-        <NotFoundPage />
-      </Box>
+      <CacheProvider value={cssCache}>
+        <Box backgroundColor={colors.bg[700]} padding="30px">
+          <NotFoundPage />
+        </Box>
+      </CacheProvider>
     );
   });
 });
